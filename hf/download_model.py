6656829e57fa2a41a349a3f76042a8974a8ed1af
Dustin <dzubke@outlook.com>
2024-02-19 18:36:03 +0000

    updated model_download and added Makefile for tgi demo

--- hf/download_model.py
+++ hf/download_model.py
@@ -1,12 +1,9 @@
 import argparse
-import joblib
-
-from huggingface_hub import hf_hub_download
-
+from huggingface_hub import snapshot_download 
 
 def download_model(repo_id):
-    local_dir = repod_id.replace("/", "_").lower()
-    hf_hub_download(repo_id=repo_id, local_dir=local_dir)
+    local_dir = repo_id.replace("/", "_").lower()
+    snapshot_download(repo_id=repo_id, local_dir=local_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
